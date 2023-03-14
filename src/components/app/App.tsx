ff7144d96508ebfda6447ace0b35e9419e58b250
Dmitry Kisel <dim03@mail.ru>
2023-03-14 14:41:10 +0300

    Refactor AppHeader

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,9 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import AppHeader from "../appHeader/AppHeader";
+import { AppHeader } from "components/appHeader";
 import { Spinner } from "components/spinner";
 
-// динамический импорт
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsPage = lazy(() => import("../pages/ComicsPage"));
--- src/components/appHeader/AppHeader.tsx
+++ src/components/appHeader/AppHeader.tsx
@@ -1,16 +1,16 @@
 import { Link, NavLink } from "react-router-dom";
 
-import "./appHeader.scss";
+import s from "./appHeader.module.scss";
 
 const AppHeader = () => {
 	return (
-		<header className='app__header'>
-			<h1 className='app__title'>
+		<header className={s.root}>
+			<h1 className={s.title}>
 				<Link to='/'>
 					<span>Marvel</span> information portal
 				</Link>
 			</h1>
-			<nav className='app__menu'>
+			<nav className={s.nav}>
 				<ul>
 					<li>
 						<NavLink end style={({ isActive }) => ({ color: isActive ? "#9f0013" : "inherit" })} to='/'>
@@ -29,4 +29,4 @@ const AppHeader = () => {
 	);
 };
 
-export default AppHeader;
+export { AppHeader };
