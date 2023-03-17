df3126b3f7617df54dabe72e876c90b7b14740ea
Dmitry Kisel <dim03@mail.ru>
2023-03-17 18:53:18 +0300

    Start implementating react query

--- src/components/charList/CharList.tsx
+++ src/components/charList/CharList.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import cx from "classnames";
+import { useQuery } from "@tanstack/react-query";
 
 import { useMarvelService } from "hooks/useMarvelService";
 
@@ -19,37 +20,30 @@ interface CharlistProps {
 }
 
 const CharList = ({ onCharSelected, selectedChar }: CharlistProps) => {
-	const [charList, setCharList] = useState<CharListType | []>([]);
-	const [isLoading, setIsLoading] = useState(false);
 	const [offset, setOffset] = useState(API_OFFSET);
 	const [charListEnded, setCharListEnded] = useState(false);
 
 	const { loading, error, getAllCharacters } = useMarvelService();
 
-	useEffect(() => {
-		onRequest(offset, true);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	const { data, refetch, isLoading, isSuccess } = useQuery({
+		queryFn: () => getAllCharacters(offset).then(onCharListLoaded),
+		queryKey: ["char"],
+	});
 
-	const onRequest = (offset: number, initial?: boolean) => {
-		initial ? setIsLoading(false) : setIsLoading(true);
-		getAllCharacters(offset).then(onCharListLoaded);
-	};
-
-	const onCharListLoaded = (loadedCharList: CharListType) => {
+	const onCharListLoaded = (result: CharListType) => {
 		let ended = false;
-		if (loadedCharList.length < 9) {
+		if (result.length < 9) {
 			ended = true;
 		}
-
-		setCharList((charList) => [...charList, ...loadedCharList]);
-		setIsLoading(false);
 		setOffset((offset) => offset + 9);
 		setCharListEnded(ended);
+
+		return result;
 	};
 
 	const handleCharClick = (id: number) => onCharSelected(id);
-	const handleBtnClick = (offset: number) => onRequest(offset);
+	// const handleBtnClick = (offset: number) => onRequest(offset);
+	const handleBtnClick = () => refetch();
 
 	if (error) return <ErrorMessage />;
 	if (loading && !isLoading) return <Spinner />;
@@ -57,20 +51,18 @@ const CharList = ({ onCharSelected, selectedChar }: CharlistProps) => {
 	return (
 		<div>
 			<ul className={s.list}>
-				{charList.map((char) => (
-					<CharItem
-						key={char.id}
-						char={char}
-						isActive={char.id === selectedChar}
-						onClick={() => handleCharClick(char.id)}
-					/>
-				))}
+				{isSuccess &&
+					data.map((char) => (
+						<CharItem
+							key={char.id}
+							char={char}
+							isActive={char.id === selectedChar}
+							onClick={() => handleCharClick(char.id)}
+						/>
+					))}
 			</ul>
 			{!charListEnded && (
-				<button
-					className={cx(s.btn, s.btnMain, s.btnLong)}
-					disabled={isLoading}
-					onClick={() => handleBtnClick(offset)}>
+				<button className={cx(s.btn, s.btnMain, s.btnLong)} disabled={isLoading} onClick={handleBtnClick}>
 					<div className={s.btnInner}>load more</div>
 				</button>
 			)}
